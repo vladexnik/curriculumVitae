8e50f398217e7bc8fcb085cd322d1d1e865dd926
anastasiya-charnahayeva <anastasiya.charnahayeva@innowise.com>
2024-07-11 12:21:15 +0200

    eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,9 +6,9 @@ module.exports = {
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier/skip-formatting',
-    "vue/multi-word-component-names"
+    '@vue/eslint-config-prettier/skip-formatting'
   ],
+  'rules': {'vue/multi-word-component-names': 'off'},
   parserOptions: {
     ecmaVersion: 'latest'
   }
