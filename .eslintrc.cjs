c82027c0c11c39af1a2e18b0f12e82513a42f3b0
anastasiya-charnahayeva <anastasiya.charnahayeva@innowise.com>
2024-07-11 09:40:27 +0200

    eslint updates

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,4 +1,3 @@
-/* eslint-env node */
 require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
@@ -7,7 +6,8 @@ module.exports = {
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier/skip-formatting'
+    '@vue/eslint-config-prettier/skip-formatting',
+    "vue/multi-word-component-names"
   ],
   parserOptions: {
     ecmaVersion: 'latest'
