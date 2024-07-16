276d41768fc1efa8f912cf00d48f0ffd6f8d311c
anastasiya-charnahayeva <anastasiya.charnahayeva@innowise.com>
2024-07-16 12:09:59 +0200

    graphql

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,16 @@ import './input.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+// Apollo client
+import { DefaultApolloClient } from '@vue/apollo-composable'
+import apolloClient from './plugins/apollo'
+
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 
+app.provide(DefaultApolloClient, apolloClient)
 app.use(createPinia())
 app.use(router)
 
--- src/views/users/Index.vue
+++ src/views/users/Index.vue
@@ -4,6 +4,36 @@
   </div>
 </template>
 
+
+<script setup lang="ts">
+  // import { storeToRefs } from "pinia";
+  // import { useAuthStore } from "../stores/modules/useAuth";
+  import { onMounted, reactive, ref } from "vue";
+  import { useRoute, useRouter } from "vue-router";
+
+  // const form = reactive({
+  //   email: "",
+  //   password: "",
+  // });
+
+  // const show_alert = ref<boolean>(false);
+  // const alert_variant = ref<string>("bg-blue-500");
+  // const alert_msg = ref<string>("Please wait! We are logging you in");
+
+  // const authStore = useAuthStore();
+  // const { user } = storeToRefs(authStore);
+  // const { signInUser } = authStore;
+  const router = useRouter();
+  const route = useRoute();
+
+
+  onMounted(() => {
+    const id = route.params.idc;
+  }
+  )
+
+</script>
+
 <style>
 @media (min-width: 1024px) {
   .about {
